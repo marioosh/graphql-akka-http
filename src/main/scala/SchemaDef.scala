48d502674889706133a9489ecedbd8f6e5d5b927
Mariusz Nosinski <mnosinski@gmail.com>
2017-06-22 18:46:53 +0200

    fixed

--- src/main/scala/SchemaDef.scala
+++ src/main/scala/SchemaDef.scala
@@ -1,8 +1,6 @@
 import sangria.execution.deferred._
 import sangria.schema.{Argument, Field, IntType, InterfaceType, ListInputType, ListType, ObjectType, OptionType, Schema, fields}
 
-import scala.concurrent.ExecutionContext.Implicits.global
-
 object SchemaDef {
 
   import Models._
@@ -16,23 +14,12 @@ object SchemaDef {
     )
   )
 
-  /**
-    * PRODUCT
-    */
-
-//  val pcRelation = Relation[Category, (Seq[Int], Category), Int]("productCategory", _._1, _._2)
-
-//
-
   implicit val ProductType: ObjectType[Unit, Product] =
     deriveObjectType[Unit, Product](
       Interfaces(IdentifiableType),
       IncludeMethods("picture") //by defaul macro cosinders fields only
     )
 
-  /**
-    * PICTURE
-    */
   implicit val PictureType: ObjectType[Unit, Picture] =
     deriveObjectType[Unit, Picture](
       ObjectTypeDescription("The product picture"),
@@ -45,24 +32,15 @@ object SchemaDef {
 
   implicit val CategoryType: ObjectType[Unit, Category] =
     deriveObjectType[Unit, Category](
-      ObjectTypeDescription("The category of products"),
-      AddFields(
-        Field("products", ListType(ProductType),
-          resolve = c => complexProdFetcher.deferRelSeq(prodComplexCat, c.value.id))
-        )
-      )
+      ObjectTypeDescription("The category of products")
+    )
 
   implicit val productHasId = HasId[Product, Int](_.id)
   implicit val categoryHasId = HasId[Category, Int](_.id)
 
 
-  val prodComplexCat = Relation[Product, (Seq[Int], Product), Int]("product-category-complex", _._1, _._2)
-//  val prodComplexCat = Relation[Product(result), (Seq[Int], Product)[tmp], Int[productId])]
-
-  val complexProdFetcher = Fetcher.relCaching[ShopRepository, Product, (Seq[Int], Product), Int](
-    (repo, ids) ⇒ repo.products(ids),
-    (repo, ids) ⇒ repo.findProductsForCategories(ids(prodComplexCat)).map(_.map(p => Seq(p.id) -> p))/**/ )
-  //.map(_.map(p ⇒ p.inCategories → p))
+  //  val prodComplexCat = Relation[Product, (Seq[Int], Product), Int]("product-category-complex", _._1, _._2)
+  //  val prodComplexCat = Relation[Product(result), (Seq[Int], Product)[tmp], Int[productId])]
 
   val Id = Argument("id", IntType)
   val Ids = Argument("ids", ListInputType(IntType))
@@ -75,7 +53,7 @@ object SchemaDef {
     (repo: ShopRepository, ids: Seq[Int]) => repo.categories(ids)
   )
 
-  lazy val deferredResolver = DeferredResolver.fetchers(complexProdFetcher, categoriesFetcher)
+  lazy val deferredResolver = DeferredResolver.fetchers(productsFetcher, categoriesFetcher)
 
   val QueryType = ObjectType(
     "Query",
@@ -98,7 +76,7 @@ object SchemaDef {
         description = Some("Returns a category with specific `id`."),
         arguments = Id :: Nil,
         resolve = c => categoriesFetcher.deferOpt(c.arg[Int]("id"))),
-      Field("categories",ListType(CategoryType),
+      Field("categories", ListType(CategoryType),
         description = Some("Returns categories by provided ids"),
         arguments = Ids :: Nil,
         resolve = c => categoriesFetcher.deferSeqOpt(c.arg[List[Int]]("ids"))
