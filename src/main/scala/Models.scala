6433690754751661c30374520fe4050d70f488b6
Mariusz Nosinski <mnosinski@gmail.com>
2017-07-11 11:18:36 +0200

    small type refactor

--- src/main/scala/Models.scala
+++ src/main/scala/Models.scala
@@ -2,13 +2,16 @@ import sangria.execution.deferred.HasId
 
 object Models {
 
+  type ProductId = Int
+  type CategoryId = Int
+
   case class Picture(width: Int, height: Int, url: Option[String])
 
   trait Identifiable {
     def id: Int
   }
 
-  case class Product(id: Int, name: String, description: String, price: BigDecimal) extends Identifiable {
+  case class Product(id: ProductId, name: String, description: String, price: BigDecimal) extends Identifiable {
     def picture(size: Int): Picture =
       Picture(width = size, height = size, url = Some(s"http://fakeimg.pl/$size/?text=ID:%20$id"))
   }
@@ -17,12 +20,12 @@ object Models {
     implicit val hasId = HasId[Product, Int](_.id)
   }
 
-  case class Category(id: String, name: String)
+  case class Category(id: CategoryId, name: String) extends Identifiable
 
   object Category {
-    implicit val hasId = HasId[Category, String](_.id)
+    implicit val hasId = HasId[Category, Int](_.id)
   }
 
-  case class Taxonomy(productId: Int, categoryId: String)
+  case class Taxonomy(productId: ProductId, categoryId: CategoryId)
 
 }
